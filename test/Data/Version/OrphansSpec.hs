8c01da4f022786a7aaf54fd013ce97a3010fd884
Ryan Scott <ryan.gl.scott@gmail.com>
2016-10-20 10:33:04 -0400

    Fix GHC build
    The output of `dataTypeOf` changed for `Version` in GHC HEAD because its `Data`
    instance was converted to a derived one.

--- test/Data/Version/OrphansSpec.hs
+++ test/Data/Version/OrphansSpec.hs
@@ -17,7 +17,12 @@ spec :: Spec
 spec = do
   describe "Data Version instance" $
     it "allows obtaining a Version constructor" $
-      dataTypeName (dataTypeOf (Version [1,2,3] [])) `shouldBe` "Data.Version.Version"
+      dataTypeName (dataTypeOf (Version [1,2,3] [])) `shouldBe`
+#if __GLASGOW_HASKELL__ >= 801
+        "Version"
+#else
+        "Data.Version.Version"
+#endif
 
 #if MIN_VERSION_base(4,7,0)
   describe "IsList Version instance" $
